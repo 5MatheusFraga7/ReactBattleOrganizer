07ad04a4d51eaada9d14d02aeac5a818d99069f8
5MatheusFraga7 <fragamatheus823@gmail.com>
2022-06-13 21:45:27 -0300

    Criando tela de login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import './Login.css'
+
+function Login() {
+    return (
+      <div class = 'container'>
+        <div class = 'form-login-container'>
+        
+        </div>
+      </div>
+    );
+  }
+  
+export default Login;
