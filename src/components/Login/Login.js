86f69b53ac8664cd725e3a3f7ba324dc96c83e6c
5MatheusFraga7 <fragamatheus823@gmail.com>
2022-06-29 19:55:45 -0300

    Adicionando a lógica da requisição de login no component de Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,70 @@
 import { Component, useEffect, useState  } from 'react';
 import './Login.css'
-import api from '../../services/api.js'
+import axios from 'axios';
 
 export default class Login extends Component {
+
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios.post("http://localhost:3333/users/sign_in",{ 
+        utf8: "✓", 
+        authenticity_token: "jX4KnKd5w8GELgXJSQzybZnnZl8oDpEfrzXhpZXm+vmBZElFLi2Q0Smg8GhQX+b8TcCUh2CsmpMKd2KMLqa+0A==", 
+        user: { 
+          email: "fragamatheus823@gmail.com", 
+          password: "123456789", 
+          remember_me: "0"
+        }, 
+        commit: "Log in"
+      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+
+  }
 
   render() {
     return (
       <div class = 'container'>
       
         <div class = 'form-login-container'>
-          <div class = 'label-login-container'>
-              <label class = 'label_login' >Log in</label>
-          </div>
 
-          <div class = 'form_container'>
-
-            <div class = 'labels-container'>
-              <label class = 'input_label' >Nome</label>
+          <form onSubmit={this.handleSubmit}>
+            <div class = 'label-login-container'>
+                <label class = 'label_login' >Log in</label>
             </div>
-            
-            <input placeholder='Insira seu nome' type="text" id="name" name="name"/>
+            <div class = 'form_container'>
 
-            <div class = 'labels-container'>
-              <label class = 'input_label' >Senha</label>
+              <div class = 'labels-container'>
+                <label class = 'input_label' >Nome</label>
+              </div>
+              
+              <input placeholder='Insira seu nome' type="text" id="name" name="name" onChange={this.handleChange} />
+
+              <div class = 'labels-container'>
+                <label class = 'input_label' >Senha</label>
+              </div>
+
+              <input placeholder='********' type="password" id="name" name="name" onChange={this.handleChange}/>
+
+              <div class = 'btn-container'>
+                <button type="submit" >Entrar</button>            
+              </div>
+              
             </div>
-
-            <input placeholder='********' type="password" id="name" name="name"/>
-
-            <div class = 'btn-container'>
-              <button type="button">Entrar</button>            
-            </div>
-            
-          </div>
+          </form>        
 
         </div>
       </div>
